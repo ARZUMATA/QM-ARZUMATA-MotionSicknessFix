0c85e91a922b4b98ce786c8b8b9a1cc1b0c54342
ARZUMATA <arzumata@gmail.com>
2025-06-19 05:40:38 +0300

    Helpers

---
+++ src/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.Security.Cryptography;
+
+internal static class Helpers
+{
+    public static string GetMd5HashFromFilePath(string filePath)
+    {
+        using (var md5 = MD5.Create())
+        using (var stream = File.OpenRead(filePath))
+        {
+            return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLowerInvariant();
+        }
+    }
+
+    public static string GetMd5HashFromStream(Stream stream)
+    {
+        using (var md5 = MD5.Create())
+        {
+            return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLowerInvariant();
+        }
+    }
+}
