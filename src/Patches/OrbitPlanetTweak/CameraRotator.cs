fe0035aac20581d3c483dd6d6f3877fa20271d33
ARZUMATA <arzumata@gmail.com>
2025-08-12 03:37:22 +0300

    fix: log flood

---
+++ src/Patches/OrbitPlanetTweak/CameraRotator.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+
+namespace QM_MotionSicknessFix
+{
+    internal partial class MotionSicknessFix
+    {
+        [HarmonyPatch(typeof(CameraRotator),
+            nameof(CameraRotator.HandleZoomInput),
+            new Type[]
+            {
+            }
+        )]
+        public static class CameraRotator_HandleZoomInput_Patch
+        {
+            public static bool Prefix(CameraRotator __instance)
+            {
+                if (__instance._framingTransposer == null)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
+
+
